89516dc310535dcfab3ab26008660008313f1ae5
Kyle Harlow <kyle.harlow@colorado.edu>
2017-10-24 18:48:21 -0600

    Added remaining null checks to conversion.c

--- src/conversion.c
+++ src/conversion.c
@@ -26,6 +26,7 @@
 
 uint8_t my_itoa(int32_t data, uint8_t * ptr, uint32_t base)
 {
+	if(ptr == NULL) return -1;
 	uint8_t i = 0;
 	uint8_t mod;
 	uint8_t sign = 0;
@@ -63,6 +64,7 @@ uint8_t my_itoa(int32_t data, uint8_t * ptr, uint32_t base)
 
 int32_t my_atoi(uint8_t * ptr, uint8_t digits, uint32_t base)
   {
+  if(ptr == NULL) return -1;
   int32_t sum = 0;
   uint8_t i;
 	/* Sum values of digit * power(Base, i) */
